45357bbe2683997c767eca5f5d950481a22a71f3
Parashara <parashara@gmail.com>
2019-01-25 23:01:36 -0800

    added switch for nomatch route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
+import NoMatch from "./components/pages/NoMatch";
 
 function App() {
   return (
     <Router>
       <div>
         <NavTabs />
+        <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/blog" component={Blog} />
         <Route path="/contact" component={Contact} />
+        <Route component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   );
