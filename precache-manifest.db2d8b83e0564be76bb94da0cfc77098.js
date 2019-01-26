424c4337ae2a298643b906920f39e9a41319a4cd
Parashara <parashara@gmail.com>
2019-01-25 19:47:36 -0800

    Updates

---
+++ precache-manifest.db2d8b83e0564be76bb94da0cfc77098.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "857926ebdfe5624d9f34",
+    "url": "/static/js/main.857926eb.chunk.js"
+  },
+  {
+    "revision": "cd1906bed11db5883921",
+    "url": "/static/js/1.cd1906be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "251c5469d8318d26ad3288e33c371354",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/My-Portfolio/precache-manifest.215dfd7ad31887122e09efc50af32f79.js"
+  "/precache-manifest.db2d8b83e0564be76bb94da0cfc77098.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/My-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
