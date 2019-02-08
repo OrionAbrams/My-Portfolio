c7c3e6f7d94527525e604a9bc32eac7a5e032a00
Parashara <parashara@gmail.com>
2019-02-07 22:17:49 -0800

    some more projects for portfolio

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,8 @@ import Rupert from "../../assets/img/rupert.png"
 import Pawstagram from "../../assets/img/pawstagram.png"
 import RPS from "../../assets/img/RPS.png"
 import Gbooks from "../../assets/img/google-books.png"
+import Fullstack from "../../assets/img/fullstack.png"
+import Scraper from "../../assets/img/scraper.png"
 import "./style.css"
 
 function Projects(props) {
@@ -12,39 +14,60 @@ function Projects(props) {
       <h1>Projects Page</h1>
       <div className="container-fluid very-high">
         <div className="row card-deck bottom-space top-space">
-          <div className="card col-md-6">
-            <Link target="_blank" to={"//pawstagram.herokuapp.com/"} >
-              <img className="project zoom card-img-top" alt="Pawstagram" src={Pawstagram} />
-            </Link>
-            <p className="card-text h-100 card-body">Pawstagram is a full stack social media web based application built for users to show off their pets. It uses  JQuery, Mysql, Sequelize, Express, Firebase, Handlebars, Material Bootstrap, and RESTful APIs. <a href="https://github.com/OrionAbrams/Pawstagram" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+          <h5 className="col-md-6 text-center">Group Projects</h5>
+          <h5 className="col-md-6 text-center">Solo Projects</h5>
+          <div className="row">
+            <div className="card col-md-6">
+              <Link target="_blank" to={"//pawstagram.herokuapp.com/"} >
+                <img className="project zoom card-img-top" alt="Pawstagram" src={Pawstagram} />
+              </Link>
+              <p className="card-text h-100 card-body">Pawstagram is a full stack social media web based application built for users to show off their pets.
+              App uses JQuery, Mysql, Sequelize, Express, Firebase, Handlebars, Material Bootstrap, and RESTful APIs. <a href="https://github.com/OrionAbrams/Pawstagram" target="_blank" rel="noopener noreferrer">Source Code</a></p>
 
+            </div>
+            <div className="card col-md-6">
+
+              <Link target="_blank" to={"//google-books-react-orion.herokuapp.com/"} >
+                <img className="project zoom card-img-top" alt="Google Books" src={Gbooks} />
+              </Link>
+              <p className="card-text h-100 card-body">An app that searches for google books, puts them into mongo database, and user can save or delete them.
+              App uses Full MERN Stack (Mongo, Express, React, Node). <a href="https://github.com/OrionAbrams/Google-Books" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+
+            </div>
+
+            <div className="row card-deck middle-spacing">
+              <div className="card col-md-6">
+
+                <Link target="_blank"
+                  to={"//orionabrams.github.io/Multiplayer-RPS/"} >
+                  <img className="project zoom card-img-top" alt="Multiplayer Rock-Paper-Scissors" src={Fullstack} />
+                </Link>
+                <p className="card-text h-100 card-body">Fullstack-Network is a web based application that enable users affiliated with coding boot camps to organize their curriculum and increase networking engagement. App uses Express, React, Node, Sequelize, and Material Design Bootstrap. <a href="https://github.com/OrionAbrams/Fullstack-Network" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+
+              </div>
+              <div className="card col-md-6">
+                <Link target="_blank" to={"//rupert-movie-companion.herokuapp.com/"} >
+                  <img className="project zoom card-img-top" alt="Rupert, The Movie Companion" src={Scraper} />
+                </Link>
+                <p className="card-text h-100 card-body">This app scrapes the latest news about indie games from IND13 and the user can leave notes on the site they and others can see. App uses Mongo, Express, Node, and Cheerio. <a href="https://github.com/OrionAbrams/Scraper" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+              </div>
+            </div>
           </div>
-          <div className="card col-md-6">
+          <div className="row card-deck middle-spacing space-from-footer">
+            <div className="card col-md-6">
+              <Link target="_blank" to={"//rupert-movie-companion.herokuapp.com/"} >
+                <img className="project zoom card-img-top" alt="Rupert, The Movie Companion" src={Rupert} />
+              </Link>
+              <p className="card-text h-100 card-body">Rupert is a movie helper app using Firebase, Dialogflow(as a Rupert chatbot), Bootstrap, and TMDB(The Movie Database) API. <a href="http://github.com/OrionAbrams/Rupert_Movie_Companion" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+            </div>
 
-            <Link target="_blank" to={"//google-books-react-orion.herokuapp.com/"} >
-              <img className="project zoom card-img-top" alt="Google Books" src={Gbooks} />
-            </Link>
-            <p className="card-text h-100 card-body">Full MERN Stack (Mongo, Express, React, Node) app that searches for google books, puts them into mongo database, and user can save or delete them. <a href="https://github.com/OrionAbrams/Google-Books" target="_blank" rel="noopener noreferrer">Source Code</a></p>
-
-          </div>
-
-
-        </div>
-        <div className="row card-deck space-from-footer">
-          <div className="card col-md-6">
-
-            <Link target="_blank"
-              to={"//orionabrams.github.io/Multiplayer-RPS/"} >
-              <img className="project zoom card-img-top" alt="Multiplayer Rock-Paper-Scissors" src={RPS} />
-            </Link>
-            <p className="card-text h-100 card-body">Multiplayer Rock-Paper-Scissors is a simple rock, paper, scissors game using firebase to connect the user as player one or player two. It uses Javascript, JQuery, Firebase, Bootstrap, and Moment.js. <a href="https://github.com/OrionAbrams/Multiplayer-RPS" target="_blank" rel="noopener noreferrer">Source Code</a></p>
-
-          </div>
-          <div className="card col-md-6">
-            <Link target="_blank" to={"//rupert-movie-companion.herokuapp.com/"} >
-              <img className="project zoom card-img-top" alt="Rupert, The Movie Companion" src={Rupert} />
-            </Link>
-            <p className="card-text h-100 card-body">Rupert is a movie helper app using Firebase, Dialogflow(as a Rupert chatbot), Bootstrap, and TMDB(The Movie Database) API. <a href="http://github.com/OrionAbrams/Rupert_Movie_Companion" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+            <div className="card col-md-6">
+              <Link target="_blank"
+                to={"//orionabrams.github.io/Multiplayer-RPS/"} >
+                <img className="project zoom card-img-top" alt="Multiplayer Rock-Paper-Scissors" src={RPS} />
+              </Link>
+              <p className="card-text h-100 card-body">Multiplayer Rock-Paper-Scissors is a simple rock, paper, scissors game using firebase to connect the user as player one or player two. It uses Javascript, JQuery, Firebase, Bootstrap, and Moment.js. <a href="https://github.com/OrionAbrams/Multiplayer-RPS" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+            </div>
           </div>
         </div>
       </div>
