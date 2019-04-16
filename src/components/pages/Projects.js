a1a457e92d7b87b5ec84983c5ba2015b8119d744
Parashara <parashara@gmail.com>
2019-04-15 22:22:58 -0700

    added stealth project

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import Rupert from "../../assets/img/rupert.png"
 import Pawstagram from "../../assets/img/pawstagram.png"
 import Pilates from "../../assets/img/baypilates.png"
-import Gbooks from "../../assets/img/google-books.png"
 import Fullstack from "../../assets/img/fullstack.png"
 import Scraper from "../../assets/img/scraper.png"
+import Stealth from "../../assets/img/stealth.png"
 import "./style.css"
 
 function Projects(props) {
@@ -19,13 +19,14 @@ function Projects(props) {
           <h5 className="col-md-6 text-center">Solo Projects</h5> */}
           <div className="row">
             <div className="card col-md-6">
-              <Link target="_blank" to={"//pawstagram.herokuapp.com/"} >
-                <img className="project zoom card-img-top" alt="Pawstagram" src={Pawstagram} />
+
+              <Link target="_blank" to={"//producer-contracts.herokuapp.com/"} >
+                <img className="project zoom card-img-top" alt="Producer Contracts" src={Stealth} />
               </Link>
-              <p className="card-text h-100 card-body">Pawstagram is a full stack social media web based application built for users to show off their pets.
-              App uses JQuery, Mysql, Sequelize, Express, Firebase, Handlebars, Material Bootstrap, and RESTful APIs. <a href="https://github.com/OrionAbrams/Pawstagram" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+              <p className="card-text h-100 card-body">A secure form-filling application that takes in user info, puts it in MongoDB, and automatically fills in a contract for the user to sign using Hellosign. Built with react so that no one can see what is on the private forms without a valid username and password. Real-world project made for a SF stealth startup. The app uses the Full MERN Stack (Mongo, Express, React, and Node). <a href="https://github.com/OrionAbrams/stealth-project" target="_blank" rel="noopener noreferrer">Source Code</a></p>
 
             </div>
+
             <div className="card col-md-6">
 
               <Link target="_blank" to={"//baypilateswellness.com"} >
@@ -47,10 +48,12 @@ function Projects(props) {
 
               </div>
               <div className="card col-md-6">
-                <Link target="_blank" to={"//rupert-movie-companion.herokuapp.com/"} >
-                  <img className="project zoom card-img-top" alt="Rupert, The Movie Companion" src={Scraper} />
+                <Link target="_blank" to={"//pawstagram.herokuapp.com/"} >
+                  <img className="project zoom card-img-top" alt="Pawstagram" src={Pawstagram} />
                 </Link>
-                <p className="card-text h-100 card-body">This app scrapes the latest news about indie games from IND13 and the user can leave notes on the site they and others can see. App uses Mongo, Express, Node, and Cheerio. <a href="https://github.com/OrionAbrams/Scraper" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+                <p className="card-text h-100 card-body">Pawstagram is a full stack social media web based application built for users to show off their pets.
+              App uses JQuery, Mysql, Sequelize, Express, Firebase, Handlebars, Material Bootstrap, and RESTful APIs. <a href="https://github.com/OrionAbrams/Pawstagram" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+
               </div>
             </div>
           </div>
@@ -62,14 +65,12 @@ function Projects(props) {
               <p className="card-text h-100 card-body">Rupert is a movie helper app using Firebase, Dialogflow(as a Rupert chatbot), Bootstrap, and TMDB(The Movie Database) API. <a href="http://github.com/OrionAbrams/Rupert_Movie_Companion" target="_blank" rel="noopener noreferrer">Source Code</a></p>
             </div>
 
+
             <div className="card col-md-6">
-
-              <Link target="_blank" to={"//google-books-react-orion.herokuapp.com/"} >
-                <img className="project zoom card-img-top" alt="Google Books" src={Gbooks} />
+              <Link target="_blank" to={"//rupert-movie-companion.herokuapp.com/"} >
+                <img className="project zoom card-img-top" alt="Rupert, The Movie Companion" src={Scraper} />
               </Link>
-              <p className="card-text h-100 card-body">An app that searches for google books, puts them into mongo database, and user can save or delete them.
-App uses Full MERN Stack (Mongo, Express, React, Node). <a href="https://github.com/OrionAbrams/Google-Books" target="_blank" rel="noopener noreferrer">Source Code</a></p>
-
+              <p className="card-text h-100 card-body">This app scrapes the latest news about indie games from IND13 and the user can leave notes on the site they and others can see. App uses Mongo, Express, Node, and Cheerio. <a href="https://github.com/OrionAbrams/Scraper" target="_blank" rel="noopener noreferrer">Source Code</a></p>
             </div>
           </div>
         </div>
